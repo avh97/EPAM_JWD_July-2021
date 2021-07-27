dc492a138b97c64e05205b2ed1dea3d4fb237dc6
avh97 <avh.1997@yandex.by>
2021-07-27 13:33:09 +0300

    add log4j2 logging via Maven

--- task01LinearPrograms/src/main/java/by/khaletski/task01/runner/Runner.java
+++ task01LinearPrograms/src/main/java/by/khaletski/task01/runner/Runner.java
@@ -1,10 +1,18 @@
 package by.khaletski.task01.runner;
 
 import by.khaletski.task01.util.TaskSwitcher;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Runner {
+
+    final static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
+        logger.info("Application launched");
         TaskSwitcher taskSwitcher = new TaskSwitcher();
         taskSwitcher.startApplication();
+        logger.info("Application successfully executed");
+
     }
 }
