c1075b3f84341dbe53e78f4ee487e668d01b9f34
avh97 <avh.1997@yandex,by>
2021-09-22 23:12:05 +0300

    add task07Composite

---
+++ task07/src/main/java/by/khaletski/task07/view/Runner.java
@@ -0,0 +1,13 @@
+package by.khaletski.task07.view;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Runner {
+
+    static final Logger LOGGER = LogManager.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+
+    }
+}
